16f9232c6d338d5d4d67f4a2d59cb801108952c1
Goutham <Gouthamsankar@aol.com>
2026-07-17 09:08:38 -0500

    Auto-save (v3.2)

--- include/hypercore/version.hpp
+++ include/hypercore/version.hpp
@@ -7,10 +7,10 @@
 namespace hypercore {
 
 inline constexpr int VERSION_MAJOR = 3;
-inline constexpr int VERSION_MINOR = 1;
+inline constexpr int VERSION_MINOR = 2;
 inline constexpr int VERSION_PATCH = 0;
 
-inline constexpr const char* VERSION_STRING = "3.1.0";
+inline constexpr const char* VERSION_STRING = "3.2.0";
 inline constexpr const char* PROJECT_NAME   = "HyperCore";
 
 } // namespace hypercore
